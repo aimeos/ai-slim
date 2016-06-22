7bddac76264891b38aa0ede78729525c0b47864b
Aimeos <aimeos@aimeos.org>
2016-06-22 18:32:56 +0200

    Adapted to Aimeos coding style

--- lib/custom/src/Slim/Router.php
+++ lib/custom/src/Slim/Router.php
@@ -22,68 +22,68 @@ use InvalidArgumentException;
  */
 class Router extends \Slim\Router
 {
-    /**
-     * Build the path for a named route excluding the base path
-     *
-     * @param string $name        Route name
-     * @param array  $data        Named argument replacement data
-     * @param array  $queryParams Optional query string parameters
-     *
-     * @return string
-     *
-     * @throws RuntimeException         If named route does not exist
-     * @throws InvalidArgumentException If required data not provided
-     */
-    public function relativePathFor($name, array $data = [], array $queryParams = [])
-    {
-        $route = $this->getNamedRoute($name);
-        $pattern = $route->getPattern();
+	/**
+	 * Build the path for a named route excluding the base path
+	 *
+	 * @param string $name		Route name
+	 * @param array  $data		Named argument replacement data
+	 * @param array  $queryParams Optional query string parameters
+	 *
+	 * @return string
+	 *
+	 * @throws RuntimeException		 If named route does not exist
+	 * @throws InvalidArgumentException If required data not provided
+	 */
+	public function relativePathFor($name, array $data = [], array $queryParams = [])
+	{
+		$route = $this->getNamedRoute($name);
+		$pattern = $route->getPattern();
 
-        $routeDatas = $this->routeParser->parse($pattern);
-        // $routeDatas is an array of all possible routes that can be made. There is
-        // one routedata for each optional parameter plus one for no optional parameters.
-        //
-        // The most specific is last, so we look for that first.
-        $routeDatas = array_reverse($routeDatas);
+		$routeDatas = $this->routeParser->parse($pattern);
+		// $routeDatas is an array of all possible routes that can be made. There is
+		// one routedata for each optional parameter plus one for no optional parameters.
+		//
+		// The most specific is last, so we look for that first.
+		$routeDatas = array_reverse($routeDatas);
 
-        $segments = $segmentKeys = [];
-        foreach ($routeDatas as $routeData) {
-            foreach ($routeData as $item) {
-                if (is_string($item)) {
-                    // this segment is a static string
-                    $segments[] = $item;
-                    continue;
-                }
+		$segments = $segmentKeys = [];
+		foreach ($routeDatas as $routeData) {
+			foreach ($routeData as $item) {
+				if (is_string($item)) {
+					// this segment is a static string
+					$segments[] = $item;
+					continue;
+				}
 
-                // This segment has a parameter: first element is the name
-                if (!array_key_exists($item[0], $data)) {
-                    // we don't have a data element for this segment: cancel
-                    // testing this routeData item, so that we can try a less
-                    // specific routeData item.
-                    $segments = [];
-                    $segmentName = $item[0];
-                    break;
-                }
-                $segments[] = $data[$item[0]];
-                $segmentKeys[$item[0]] = true;
-            }
-            if (!empty($segments)) {
-                // we found all the parameters for this route data, no need to check
-                // less specific ones
-                break;
-            }
-        }
+				// This segment has a parameter: first element is the name
+				if (!array_key_exists($item[0], $data)) {
+					// we don't have a data element for this segment: cancel
+					// testing this routeData item, so that we can try a less
+					// specific routeData item.
+					$segments = [];
+					$segmentName = $item[0];
+					break;
+				}
+				$segments[] = $data[$item[0]];
+				$segmentKeys[$item[0]] = true;
+			}
+			if (!empty($segments)) {
+				// we found all the parameters for this route data, no need to check
+				// less specific ones
+				break;
+			}
+		}
 
-        if (empty($segments)) {
-            throw new InvalidArgumentException('Missing data for URL segment: ' . $segmentName);
-        }
-        $url = implode('', $segments);
+		if (empty($segments)) {
+			throw new InvalidArgumentException('Missing data for URL segment: ' . $segmentName);
+		}
+		$url = implode('', $segments);
 
-        $params = array_merge(array_diff_key($data, $segmentKeys), $queryParams);
-        if ($params) {
-            $url .= '?' . http_build_query($params);
-        }
+		$params = array_merge(array_diff_key($data, $segmentKeys), $queryParams);
+		if ($params) {
+			$url .= '?' . http_build_query($params);
+		}
 
-        return $url;
-    }
+		return $url;
+	}
 }
